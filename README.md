33bd9dda7dc7bcbf7a57736050d8010aed047c54
Aleksey Chirkin <a4irkin@gmail.com>
2022-11-25 16:20:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# VSCode SQL Comment Code Block Grammar Injection
+
+Now supports this type of blocks: yaml, json, html, markdown.
+
+![before/after](/assets/before_after.png)
+
---
+++ test.sql
@@ -0,0 +1,31 @@
+
+SELECT 1;
+
+/*
+multi-line
+comment
+*/
+
+/* yaml
+property: value
+*/
+
+/* json
+{
+  "property1": "value",
+  "property2": {
+    "property3": "value"
+  }
+}
+*/
+
+/* html
+<div>Hello world</div>
+*/
+
+/* markdown
+***some text***
+*/
+
+
+
